56a10675df9c9ae484ff6bc8863cd77d6275c2e7
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-08-13 18:32:46 -0300

    First Commit

---
+++ flask/app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return render_template("jinja_intro.html", name="Bob Smith", template_name="Jinja2")
