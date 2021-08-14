19f37961947e3f74b357bc87580bf19cf188fd70
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-08-14 16:46:20 -0300

    Data structures

--- flask/app.py
+++ flask/app.py
@@ -3,28 +3,34 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/expressions/")
-def hello_world():
+class GalileanMoons:
+    def __init__(self, first, second, third, fourth):
+        self.first = first
+        self.second = second
+        self.third = third
+        self.fourth = fourth
 
-    color = "brown"
-    animal_one = "fox"
-    animal_two = "dog"
 
-    orange_amount = 30
-    apple_amount = 20
-    donate_amount = 15
+@app.route("/data-structures")
+def render_data_structures():
 
-    first_name = "Jaden"
-    last_name = "Yuki"
+    movies = [
+        "Leon, The Professional",
+        "The Usual Suspects",
+        "A Beautiful Mind"
+    ]
+
+    car = {
+        "brand": "Tesla",
+        "model": "Roadster",
+        "year": "2020"
+    }
+
+    moons = GalileanMoons("Io", "Europa", "Ganymede", "Callisto")
 
     kwargs = {
-        "color": color,
-        "animal_one": animal_one,
-        "animal_two": animal_two,
-        "orange_amount": orange_amount,
-        "apple_amount": apple_amount,
-        "donate_amount": donate_amount,
-        "first_name": first_name,
-        "last_name": last_name
+        "movies": movies,
+        "car": car,
+        "moons": moons
     }
-    return render_template("expressions.html", **kwargs)
+    return render_template("data_structures.html", **kwargs)
