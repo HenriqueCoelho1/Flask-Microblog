053bd935210e399f4f4b72ba6ea9093ba794fc7a
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-08-14 15:59:19 -0300

    Expressions

--- flask/app.py
+++ flask/app.py
@@ -3,6 +3,28 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/expressions/")
 def hello_world():
-    return render_template("jinja_intro.html", name="Bob Smith", template_name="Jinja2")
+
+    color = "brown"
+    animal_one = "fox"
+    animal_two = "dog"
+
+    orange_amount = 30
+    apple_amount = 20
+    donate_amount = 15
+
+    first_name = "Jaden"
+    last_name = "Yuki"
+
+    kwargs = {
+        "color": color,
+        "animal_one": animal_one,
+        "animal_two": animal_two,
+        "orange_amount": orange_amount,
+        "apple_amount": apple_amount,
+        "donate_amount": donate_amount,
+        "first_name": first_name,
+        "last_name": last_name
+    }
+    return render_template("expressions.html", **kwargs)
