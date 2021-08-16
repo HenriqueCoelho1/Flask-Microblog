fa573fc85be9e00567d5785678663fb1b7c66735
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-08-16 17:52:10 -0300

    conditionals basics

--- flask/app.py
+++ flask/app.py
@@ -3,34 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-class GalileanMoons:
-    def __init__(self, first, second, third, fourth):
-        self.first = first
-        self.second = second
-        self.third = third
-        self.fourth = fourth
-
-
-@app.route("/data-structures")
-def render_data_structures():
-
-    movies = [
-        "Leon, The Professional",
-        "The Usual Suspects",
-        "A Beautiful Mind"
-    ]
-
-    car = {
-        "brand": "Tesla",
-        "model": "Roadster",
-        "year": "2020"
-    }
-
-    moons = GalileanMoons("Io", "Europa", "Ganymede", "Callisto")
-
-    kwargs = {
-        "movies": movies,
-        "car": car,
-        "moons": moons
-    }
-    return render_template("data_structures.html", **kwargs)
+@app.route("/conditionals-basics")
+def render_conditionals_basics():
+    company = "Microsoft"
+    return render_template("conditional_basics.html", company=company)
