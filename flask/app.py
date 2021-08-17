3be60dd91de0a888999a83eca599b839b2868795
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-08-17 18:52:10 -0300

    Retriving data

--- flask/app.py
+++ flask/app.py
@@ -1,15 +1,32 @@
 import datetime
 from flask import Flask, render_template, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
-
+client = MongoClient(
+    "mongodb+srv://batata:batata123@microblog.5r1dz.mongodb.net/test")
+app.db = client.microblog
 entries = []
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    print([e for e in app.db.entries.find({})])
     if request.method == "POST":
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
         entries.append((entry_content, formatted_date))
-    return render_template("index.html", entries=entries)
+        app.db.entries.insert(
+            {"content": entry_content, "date": formatted_date})
+
+    entries_with_date = [
+        (
+            entry[0],
+            entry[1],
+            datetime.datetime.strptime(
+                entry[1], "%Y-%m-%d").strftime("%b %d")
+        )
+
+        for entry in entries
+    ]
+    return render_template("index.html", entries=entries_with_date)
